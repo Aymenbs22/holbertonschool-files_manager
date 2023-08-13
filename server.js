670f0d362ebec8b96c4ba84aa51deee6946946a7
Aymenbs22 <aymenbenslima0@gmail.com>
2023-08-13 17:13:31 +0100

    should listen on the port set by the environment variable PORT or by default 5000 it should load all routes from the file routes/index.js Inside the folder routes, create a file index.js that contains all endpoints of our API:
    GET /status => AppController.getStatus
    GET /stats => AppController.getStats
    Inside the folder controllers, create a file AppController.js that contains the definition of the 2 endpoints:
    
    GET /status should return if Redis is alive and if the DB is alive too by using the 2 utils created previously: { redis: true, db: true } with a status code 200
    GET /stats should return the number of users and files in DB: { users: 12, files: 1231 } with a status code 200
    users collection must be used for counting all users
    files collection must be used for counting all

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const indexRoutes    = require("./routes/index.js");
+
+app.use("/", indexRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
