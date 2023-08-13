f4f0e2c80f059871d5c44adf0cea380b2b030d19
Aymenbs22 <aymenbenslima0@gmail.com>
2023-08-13 16:16:32 +0100

    the constructor that creates a client to MongoDB: host: from the environment variable DB_HOST or default: localhost port: from the environment variable DB_PORT or default: 27017 database: from the environment variable DB_DATABASE or default: files_manager a function isAlive that returns true when the connection to MongoDB is a success otherwise, false an asynchronous function nbUsers that returns the number of documents in the collection users an asynchronous function nbFiles that returns the number of documents in the collection files After the class definition, create and export an instance of DBClient called dbClient

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+class DBClient {
+    constructor(
+        host = process.env.DB_HOST || 'localhost',
+        port = process.env.DB_PORT || '27017',
+        database = process.env.DB_DATABASE || "files_manager",
+        url = `mongodb://${host}:${port}`,
+    ){
+    MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+        if (!err) {
+            this.db = database;
+        }
+    });
+
+    }
+    isAlive(){
+        if (this.database) return true;
+        return false;
+    }
+
+    nbUsers (){
+        return this.users.countDocuments({});
+    }
+
+    nbFiles (){
+        return this.files.countDocuments({});
+    }
+}
+
+module.exports = new DBClient();
