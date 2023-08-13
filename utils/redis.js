b5bbb23b0337d188c7f91a7c09f072bdebfa4678
Aymenbs22 <aymenbenslima0@gmail.com>
2023-08-13 14:08:59 +0100

    the constructor that creates a client to Redis:

--- utils/redis.js
+++ utils/redis.js
@@ -1,32 +1,29 @@
-const redis = require('redis');
 const { promisify } = require('util');
+const redis = require('redis');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (err) => console.log('Redis Client Error', err));
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+  }
 
-    isAlive(){
-        const alive = this.client.isAlive = true;
-        if (alive) {
-            return alive;
-        } else {
-            return false;
-        }
-    }
+  isAlive() {
+    this.client.isAlive = true;
+    const alive = this.client.isAlive;
+    return alive;
+  }
 
-    async get(key){
-        const getv = promisify(this.client.get).bind(this.client)(key);
-        return getv;
-    }
+  async get(key) {
+    const getv = promisify(this.client.get).bind(this.client)(key);
+    return getv;
+  }
 
-    async set(key, value, duration){
-        return this.client.set(key, value, 'EX',duration);
-    }
+  async set(key, value, duration) {
+    return this.client.set(key, value, 'EX', duration);
+  }
 
-    async del(key){
-        return this.client.del(key);
-    }
+  async del(key) {
+    return this.client.del(key);
+  }
 }
-module.exports = new RedisClient;
+module.exports = new RedisClient();
