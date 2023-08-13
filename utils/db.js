731be68be9d6612030e410e578b63d27488fb43b
Aymenbs22 <aymenbenslima0@gmail.com>
2023-08-13 16:36:29 +0100

    the constructor that creates a client to MongoDB: host: from the environment variable DB_HOST or default: localhost port: from the environment variable DB_PORT or default: 27017 database: from the environment variable DB_DATABASE or default: files_manager a function isAlive that returns true when the connection to MongoDB is a success otherwise, false an asynchronous function nbUsers that returns the number of documents in the collection users an asynchronous function nbFiles that returns the number of documents in the collection files After the class definition, create and export an instance of DBClient called dbClient

--- utils/db.js
+++ utils/db.js
@@ -1,30 +1,31 @@
 const { MongoClient } = require('mongodb');
+
 class DBClient {
-    constructor(
-        host = process.env.DB_HOST || 'localhost',
-        port = process.env.DB_PORT || '27017',
-        database = process.env.DB_DATABASE || "files_manager",
-        url = `mongodb://${host}:${port}`,
-    ){
-    MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
-        if (!err) {
-            this.db = database;
-        }
+  constructor(
+    host = process.env.DB_HOST || 'localhost',
+    port = process.env.DB_PORT || '27017',
+    database = process.env.DB_DATABASE || 'files_manager',
+    url = `mongodb://${host}:${port}`,
+  ) {
+    MongoClient.connect(url, { native_parser: true }, (err, db) => {
+      if (!err) {
+        this.db = db.db(database);
+      }
     });
+  }
 
-    }
-    isAlive(){
-        if (this.database) return true;
-        return false;
-    }
+  isAlive() {
+    if (this.database) return true;
+    return false;
+  }
 
-    nbUsers (){
-        return this.users.countDocuments({});
-    }
+  nbUsers() {
+    return this.users.countDocuments({});
+  }
 
-    nbFiles (){
-        return this.files.countDocuments({});
-    }
+  nbFiles() {
+    return this.files.countDocuments({});
+  }
 }
 
 module.exports = new DBClient();
