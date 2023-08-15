5efe9e1bc1dc49d11173f7f06fc22d0e615d0c93
Aymenbs22 <aymenbenslima0@gmail.com>
2023-08-15 15:33:38 +0100

    have a simple API, it’s time to add users to our database

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,30 +1,31 @@
 const express = require('express');
+
 const router = express.Router();
+const sha1 = require('sha1');
 const db = require('../utils/db');
-var sha1 = require('sha1');
 
+router.post('/users', async (req, res) => {
+  const { password } = req.body;
+  const { email } = req.body;
+  const { User } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
+  const emailfind = await db.findOne({ email });
 
-    router.post('/users', async (req, res) => {
-    const { password } = req.body;
-    const { email } = req.body;
-    if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-    }
-    if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-    }
-    const emailfind = await db.findOne({ email });
+  if (emailfind) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+  const hashedpassword = sha1(password);
 
-    if (emailfind) {
-        return res.status(400).json({ error: 'Already exist' });
-    }
-    const hashedpassword = sha1(password);
+  const newUser = new User({
+    email,
+    password: hashedpassword,
+  });
 
-    const newUser = new User({
-        email,
-        password: hashedpassword,
-      });
-
-    await newUser.save();
-    return res.status(201).json({ email: newUser.email, id: newUser._id });
-})
+  await newUser.save();
+  return res.status(201).json({ email: newUser.email, id: newUser._id });
+});
